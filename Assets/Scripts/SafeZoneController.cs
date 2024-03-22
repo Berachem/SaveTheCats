8ad75c136eb489088af384dfd1ee0ed46188a366
Berachem Markria <berachem.markria@gmail.com>
2024-03-22 14:33:06 +0100

    UI cats remaining to save !

--- Assets/Scripts/SafeZoneController.cs
+++ Assets/Scripts/SafeZoneController.cs
@@ -1,9 +1,19 @@
-using UnityEditorInternal;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class SafeZoneController : MonoBehaviour
 {
+    public TextMeshProUGUI indication;
+    private int catsRemaining;
+
+    private void Start()
+    {
+        // Initialiser catsRemaining avec le nombre de chats dans la scène
+        catsRemaining = GameObject.FindGameObjectsWithTag("Grabable").Length;
+        UpdateIndicationText();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Grabable"))
@@ -15,6 +25,10 @@ public class SafeZoneController : MonoBehaviour
             catController.GetComponent<Rigidbody>().isKinematic = false;
             catController.GetComponent<Animator>().SetBool("Walk", false);
             catController.GetComponent<Animator>().SetBool("Sit", true);
+
+            // Décrémente catsRemaining et met à jour le texte
+            catsRemaining--;
+            UpdateIndicationText();
         }
     }
 
@@ -29,6 +43,24 @@ public class SafeZoneController : MonoBehaviour
             catController.GetComponent<Rigidbody>().isKinematic = true;
             catController.GetComponent<Animator>().SetBool("Walk", true);
             catController.GetComponent<Animator>().SetBool("Sit", false);
+
+            // Incrémente catsRemaining et met à jour le texte
+            catsRemaining++;
+            UpdateIndicationText();
+        }
+    }
+
+    private void UpdateIndicationText()
+    {
+        if (catsRemaining > 0)
+        {
+            // Texte avec X en rouge
+            indication.text = $"Encore <color=#FF0000>{catsRemaining}</color> chats à sauver !";
+        }
+        else
+        {
+            // Texte "Victoire !" en vert
+            indication.text = "<color=#00FF00>Victoire !</color>";
         }
     }
 }

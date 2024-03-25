f06400d29c358468740280b50195fc03cfeeb04b
Kilouhardi <Joshual.courriel@gmail.com>
2024-03-25 23:02:21 +0100

    Start Menu

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Medieval");
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}
